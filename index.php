a9d8164c6cdd9dccc922de93a2d82b4dae3958dc
HudaHK2000 <eng.huda.kabbani@gmail.com>
2024-01-09 10:01:41 +0200

    test checkout

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<h1>Hello github</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorum iste earum maiores? Distinctio quaerat harum odit facilis culpa nesciunt reprehenderit dignissimos veritatis iure, ea, assumenda impedit, accusantium ipsam numquam.</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Hello github</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorum iste earum maiores? Distinctio quaerat harum odit facilis culpa nesciunt reprehenderit dignissimos veritatis iure, ea, assumenda impedit, accusantium ipsam numquam.</p>
+    <ul>
+        <li>o</li>
+    </ul>    
+</body>
+</html>
